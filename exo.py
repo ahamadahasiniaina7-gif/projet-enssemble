1664b02f9199f5c203706a157d5a94b99d6b03b4
AHAMADA <ahamadahasiniaina7@gmail.com>
2026-07-03 09:20:06 +0300

    nanamboatra dependance

--- exo.py
+++ exo.py
@@ -1,36 +1,7 @@
 #ataovy ny devoir
-class Employee:
-    def __init__(self, id, nom, poste, salaire):
-        self.id = id
-        self.nom = nom
-        self.poste = poste
-        self.salaire = salaire
-
-    def display_info(self):
-        print(f"ID : {self.id}")
-        print(f"Nom : {self.nom}")
-        print(f"Poste : {self.poste}")
-        print(f"Salaire : {self.salaire:,} Ar")
-
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "nom": self.nom,
-            "poste": self.poste,
-            "salaire": self.salaire
-        }
 
 
-
-employe1 = Employee(1, "Jean", "Développeur", 1500000)
-
-employe1.display_info()
-
-
-print("\nDictionnaire :")
-print(employe1.to_dict())
-
-#exercice 2
+"""#exercice 2
 class EmployeeManager:
     def __init__(self):
         self.employees = []
@@ -105,4 +76,4 @@ class EmployeeManager:
 
     def save_to_json(self):
         with open("employees.json", "w", encoding="utf-8") as file:
-            json.dump(self.employees, file, ensure_ascii=False, indent=4)
+            json.dump(self.employees, file, ensure_ascii=False, indent=4)"""
